7747abe5d04d7f7a7e6652da23af19de1f7a0c08
iliasbendali <ilias.bendali@etu.minesparis.psl.eu>
2025-01-31 16:16:54 +0100

    Update ilias.py

--- ilias.py
+++ ilias.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import heapq
 
 clients = pd.read_csv('C:/Users/Ilias/OneDrive/Desktop/MINES 1A/info/hackathon 2/Hackathon/clients.csv')
 usines = pd.read_csv('C:/Users/Ilias/OneDrive/Desktop/MINES 1A/info/hackathon 2/Hackathon/plants.csv')
