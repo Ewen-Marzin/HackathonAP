8e83555f70c3b90ac661524a688106de27c52a83
Ilias Bendali <ilias.bendali@etu.minesparis.psl.eu>
2025-01-31 16:28:18 +0100

    version finale (j'espère)

--- ilias.py
+++ ilias.py
@@ -8,23 +8,25 @@ clients["statut"] = 1
 
 # utilisation d'une file de priorité pour lister les différents évènements 
 
+import heapq
+
 v=0.05 # vitesse en km/h
 
 def évènements(camions):
     pq = []
     déplacements_camions=[[(camions[i][0],camions[i][1])] for i in range(100)]
     t=0
-    while t < 30*24 : 
-        for i in range(100): #initialisation pour tous les camtards
-            (x1,y1) = camions[i][0],camions[i][1]
-            x = arrivee_camion(camions[i])
-            if x[4] = None : 
-                (x2,y2) = (clients[x[5]][0], clients[x[5]][1])
-            else : 
-                (y2,x2) = (usines[x[4]][0], usines[x[4]][1])
-            déplacements_camions[i].append((x2,y2))
-            y = distance(x1,y1,x2,y2)/v
-            heapq.heappush(y,i)
+    for i in range(100): #initialisation pour tous les camtards
+        (x1,y1) = camions[i][0],camions[i][1]
+        x = arrivee_camion(camions[i])
+        if x[4] = None : 
+            (x2,y2) = (clients[x[5]][0], clients[x[5]][1])
+        else : 
+            (y2,x2) = (usines[x[4]][0], usines[x[4]][1])
+        déplacements_camions[i].append((x2,y2))
+        y = distance(x1,y1,x2,y2)/v
+        heapq.heappush(y,i)
+    while t < 30*24 :
         (t,i) = heapq.heappop(pq)
         x = arrivee_camion(camions[i])
         (x1,y1) = camions[i][0],camions[i][1]
@@ -32,8 +34,19 @@ def évènements(camions):
             (x2,y2) = (clients[x[5]][0], clients[x[5]][1])
         else : 
             (y2,x2) = (usines[x[4]][0], usines[x[4]][1])
+        déplacements_camions[i].append((x2,y2))
         y = distance(x1,y1,x2,y2)/v
         t+= y
         heapq.heappush(t,i)
+    for i in range(100):
+        (t,i) = heapq.heappop(pq)
+        x = arrivee_camion(camions[i])
+        (x1,y1) = camions[i][0],camions[i][1]
+        if x[4] = None : 
+            (x2,y2) = (clients[x[5]][0], clients[x[5]][1])
+        else : 
+            (y2,x2) = (usines[x[4]][0], usines[x[4]][1])
+        déplacements_camions[i].append((x2,y2))
+        y = distance(x1,y1,x2,y2)/v
     print("finitooooooo")
     return déplacements_camions
