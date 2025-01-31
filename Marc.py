058504312f39e26a061f70edf4e2339a2c7a4820
MarcZhg <marc91.perso@gmail.com>
2025-01-31 10:49:46 +0100

    Clock

--- Marc.py
+++ Marc.py
@@ -1,5 +1,11 @@
 import numpy as np
+import pygame as pg
+
+pg.init()
+clock = pg.time.Clock()
+running = True
+while running:
+    clock.tick(10)
 
 def distance(x1, y1, x2, y2):
     return np.sqrt((x2 - x1)**2 + (y2 - y1)**2)
-
